96c6b55cc6a37f1f06fc61e84965406571ce4eac
afeiship <1290657123@qq.com>
2022-03-01 13:40:54 +0800

    feat: initail

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Command, flags } from "@oclif/command";
+import fs from "fs";
+import sitemap from "@jswork/sitemap";
+
+class SitemapCli extends Command {
+  static description = "Generate sitemap with nodejs.";
+
+  static flags = {
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    limit: flags.string({
+      char: "l",
+      description: "The limit of single file sitemap size.",
+    }),
+  };
+
+  static args = [{ name: "filename" }];
+
+  async run() {
+    const { args, flags } = this.parse(SitemapCli);
+    const filename = args.filename || "urls.txt";
+    const urls = fs.readFileSync(filename, "utf8").split("\n");
+
+    sitemap(urls, { limit: flags.limit || 45000 });
+    this.log("😎 sitemap generated.");
+  }
+}
+
+export = SitemapCli;
